99b44c064d6c400139162a27cd9c80d475bc71c3
falentio <riv.kevinfalent.io@gmail.com>
2022-11-19 14:46:53 +0700

    chore(types): change folder structure

--- cmd/codegen/types.ts
+++ cmd/codegen/types.ts
@@ -6,7 +6,6 @@ const enumsTmpl = handlebars.compile(`package types
 
 const (
 	None TypeBit = 1 << iota >> 1
-
 	{{#each types}}
 	{{this}}
 	{{/each}}
@@ -15,16 +14,20 @@ const (
 
 const typesTmpl = handlebars.compile(`package types
 
+import (
+	"github.com/falentio/poke/types"
+)
+
 func init() {
-	Register({{type}}Data)
+	types.Register({{type}}Data)
 }
 
-var {{type}}Data TypeData = TypeData{
-	Bit: {{type}},
+var {{type}}Data types.TypeData = types.TypeData{
+	Bit: types.{{type}},
 
-	DoubleDamageTaken: None{{#each doubleDamageTaken}} | {{this}}{{/each}},
-	HalfDamageTaken:   None{{#each halfDamageTaken}} | {{this}}{{/each}},
-	ZeroDamageTaken:   None{{#each zeroDamageTaken}} | {{this}}{{/each}},
+	DoubleDamageTaken: types.None{{#each doubleDamageTaken}} | types.{{this}}{{/each}},
+	HalfDamageTaken:   types.None{{#each halfDamageTaken}} | types.{{this}}{{/each}},
+	ZeroDamageTaken:   types.None{{#each zeroDamageTaken}} | types.{{this}}{{/each}},
 }
 `);
 
@@ -32,26 +35,26 @@ let { results } = await pokeapi("/api/v2/type");
 results = results.filter((i) => !["shadow", "unknown"].includes(i.name));
 
 const code = enumsTmpl({
-  types: results
-    .map((i) => pascalCase(i.name)),
+	types: results
+		.map((i) => pascalCase(i.name)),
 });
 await write(`types/enum.go`, code);
 await Promise.all(
-  results
-    .map(async (i) => {
-      const res = await pokeapi(i.url);
-      const code = typesTmpl({
-        type: pascalCase(i.name),
-        doubleDamageTaken: res.damage_relations.double_damage_from
-          .map((i) => pascalCase(i.name))
-          .sort((a, b) => a.localeCompare(b)),
-        halfDamageTaken: res.damage_relations.half_damage_from
-          .map((i) => pascalCase(i.name))
-          .sort((a, b) => a.localeCompare(b)),
-        zeroDamageTaken: res.damage_relations.no_damage_from
-          .map((i) => pascalCase(i.name))
-          .sort((a, b) => a.localeCompare(b)),
-      });
-      await write(`types/${i.name}.go`, code.replace(/None \| /g, ""));
-    }),
+	results
+		.map(async (i) => {
+			const res = await pokeapi(i.url);
+			const code = typesTmpl({
+				type: pascalCase(i.name),
+				doubleDamageTaken: res.damage_relations.double_damage_from
+					.map((i) => pascalCase(i.name))
+					.sort((a, b) => a.localeCompare(b)),
+				halfDamageTaken: res.damage_relations.half_damage_from
+					.map((i) => pascalCase(i.name))
+					.sort((a, b) => a.localeCompare(b)),
+				zeroDamageTaken: res.damage_relations.no_damage_from
+					.map((i) => pascalCase(i.name))
+					.sort((a, b) => a.localeCompare(b)),
+			});
+			await write(`types/data/${i.name}.go`, code.replace(/types.None \| /g, ""));
+		}),
 );
